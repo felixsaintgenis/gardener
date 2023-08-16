9bb16bf66425c89ef41ea08f6bd1c568a9320c99
felixsaintgenis <saintgenisfelix@gmail.com>
2023-08-16 18:34:08 +0200

    add signup logic

--- backend/controllers/userController.mjs
+++ backend/controllers/userController.mjs
@@ -7,5 +7,11 @@ export const loginUser = async (req, res) => {
 
 //signup user
 export const signupUser = async (req, res) => {
-  res.json({ msg: 'User is signup' });
+  const { email, password } = req.body;
+  try {
+    const user = await User.signup(email, password);
+    res.status(200).json({ email, user });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
--- backend/models/userModel.mjs
+++ backend/models/userModel.mjs
@@ -1,4 +1,6 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+import validator from 'validator';
 
 const Schema = mongoose.Schema;
 
@@ -13,5 +15,28 @@ const userSchema = new Schema({
     required: true,
   },
 });
+
+// static signup method
+
+userSchema.statics.signup = async function (email, password) {
+  if (!email || !password) {
+    throw Error('Veuillez remplir les champs requis');
+  }
+  if (!validator.isEmail(email)) {
+    throw Error("Cet email n'est pas valide");
+  }
+  if (!validator.isStrongPassword(password)) {
+    throw Error("Le mot de passe n'est pas assez sécurisé");
+  }
+  const exist = await this.findOne({ email });
+  if (exist) {
+    throw Error('Cet email est déjà utilisé par un autre compte');
+  }
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+
+  const user = await this.create({ email, password: hash });
+  return user;
+};
 
 export default mongoose.model('User', userSchema);
